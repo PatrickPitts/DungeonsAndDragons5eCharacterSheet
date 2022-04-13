1389de5c4633af688e1829b38c755decb858dc77
patri <patrick.pittsj@gmail.com>
2022-04-13 12:02:46 -0700

    Updated Feature Class Diagram, implementation

--- src/main/java/org/nerdcore/dungeonsanddragons5echaractersheet/model/FeatureAggregate/CharacterClass/CharacterClass.java
+++
@@ -1,8 +0,0 @@
-package org.nerdcore.dungeonsanddragons5echaractersheet.model.FeatureAggregate.CharacterClass;
-
-import org.nerdcore.dungeonsanddragons5echaractersheet.model.FeatureAggregate.FeatureAggregate;
-
-public abstract class CharacterClass implements FeatureAggregate {
-
-    int classLevel;
-}
---
+++ src/main/java/org/nerdcore/dungeonsanddragons5echaractersheet/model/Tester.java
@@ -0,0 +1,16 @@
+package org.nerdcore.dungeonsanddragons5echaractersheet.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tester {
+
+    static List<Feature> featureList;
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
